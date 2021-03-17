907a0c4f37d3ce57cc304a8ec835b575eb074624
burd_up <lukichev.r1@gmail.com>
2021-03-17 15:08:17 +0300

    new chenge 17.03.2021

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
 import {connect} from "react-redux";
+import {
+    changeCurrentPageActionCreator, changeTotalUserCountActionCreator,
+    followActionCreator,
+    setUsersActionCreator,
+    unfollowActionCreator
+} from "../../redux/users-reducer";
+import * as axios from "axios";
 import Users from "./Users";
-import {followActionCreator, setUsersActionCreator, unfollowActionCreator} from "../../redux/users-reducer";
 
+class UsersAPIComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount = () => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.changeTotalUserCount(response.data.totalCount);
+        });
+    }
+
+    onPageChange = (number) => {
+        this.props.changeCurrentPage(number);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        })
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      changeCurrentPage={this.props.changeCurrentPage}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unFollow={this.props.unFollow}
+                      onPageChange={this.onPageChange}/>
+    };
+}
 
 let mapStateToProps = (state) => {
-    return {users: state.usersPage.users}
+    return {
+        users: state.usersPage.users,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        pageSize: state.usersPage.pageSize,
+        currentPage: state.usersPage.currentPage
+    }
 }
 let mapDispatchToProps = (dispatch) => {
     return {
@@ -17,10 +58,16 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersActionCreator(users))
+        },
+        changeCurrentPage: (currentPage) => {
+            dispatch(changeCurrentPageActionCreator(currentPage))
+        },
+        changeTotalUserCount: (totalUserCount) => {
+            dispatch(changeTotalUserCountActionCreator(totalUserCount))
         }
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
 
 export default UsersContainer;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const CHANGE_CURRENT_PAGE = 'CHANGE_CURRENT_PAGE';
+const CHANGE_TOTAL_USER_COUNT = 'CHANGE_TOTAL_USER_COUNT';
 
 const initialState = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -29,7 +34,11 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS:
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users: [...action.users]}
+        case CHANGE_CURRENT_PAGE:
+            return {...state, currentPage: action.currentPage}
+        case CHANGE_TOTAL_USER_COUNT:
+            return {...state, totalUsersCount: action.totalUsersCount}
         default:
             return state;
     }
@@ -38,6 +47,8 @@ const usersReducer = (state = initialState, action) => {
 export const followActionCreator = (userId) => ({type: FOLLOW, userId: userId});
 export const unfollowActionCreator = (userId) => ({type: UNFOLLOW, userId: userId});
 export const setUsersActionCreator = (users) => ({type: SET_USERS, users: users});
+export const changeCurrentPageActionCreator = (currentPage) => ({type: CHANGE_CURRENT_PAGE, currentPage: currentPage});
+export const changeTotalUserCountActionCreator = (totalUsersCount) => ({type: CHANGE_TOTAL_USER_COUNT, totalUsersCount: totalUsersCount});
 
 export default usersReducer;
 
